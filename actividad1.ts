e61f70b4e011458625dc363714ec3fdc8df16247
Fer31Concept <fernandojalil276@gmail.com>
2026-09-10 13:43:35 -0600

    Datos Basicos - Complete

---
+++ actividad1.ts
@@ -0,0 +1,14 @@
+function Name(name: string): string {
+  return 'Nombre: ' + name;
+}
+console.log(Name('Fer'));
+
+function Age(age: number): string {
+    return "Edad: " + age;
+}
+console.log(Age(19));
+
+function Student(active: string): string {
+    return "Estado: " + active;
+}
+console.log(Student("Estudiante activo"));
